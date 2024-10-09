6d1e9fabdf08126c3a7c9d4153e3e9197384ac2c
ellouzatim <ellouzati.mohamed84@gmail.com>
2024-10-09 16:26:36 +0200

    correction

--- de_mutation.cu
+++ de_mutation.cu
@@ -2,28 +2,29 @@
 #include <curand_kernel.h>
 #include "kernel.h"
 
-
-__global__ void kernelDEMutation(float *individuals, int *indexMutation, float *mutants, float F)
-{
+__global__ void kernelDEMutation(float *individuals, int *indexMutation, float *mutants, float F) {
+    extern __shared__ float sharedMem[];
+    
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i >= NUM_OF_PARTICLES * NUM_OF_DIMENSIONS || i % NUM_OF_DIMENSIONS != 0) return;
-
-    int idx = i / NUM_OF_DIMENSIONS;  
-
-    int r_base = indexMutation[idx * 3];
-    int r_1 = indexMutation[idx * 3 + 1];
-    int r_2 = indexMutation[idx * 3 + 2];
-
-    if (r_base < 0 || r_base >= NUM_OF_PARTICLES ||
-        r_1 < 0 || r_1 >= NUM_OF_PARTICLES ||
-        r_2 < 0 || r_2 >= NUM_OF_PARTICLES) {
-        return;
-    }
-
+    if (i >= NUM_OF_PARTICLES) return;
+    
+    int r_base = indexMutation[i * 3];
+    int r_1 = indexMutation[i * 3 + 1];
+    int r_2 = indexMutation[i * 3 + 2];
+    
+    float *base = &sharedMem[threadIdx.x * NUM_OF_DIMENSIONS * 3];
+    float *x_r1 = &base[NUM_OF_DIMENSIONS];
+    float *x_r2 = &x_r1[NUM_OF_DIMENSIONS];
+    
     for (int d = 0; d < NUM_OF_DIMENSIONS; d++) {
-        int currentIdx = i + d;
-        mutants[currentIdx] = individuals[r_base * NUM_OF_DIMENSIONS + d] + 
-                              F * (individuals[r_1 * NUM_OF_DIMENSIONS + d] - individuals[r_2 * NUM_OF_DIMENSIONS + d]);
+        base[d] = individuals[r_base * NUM_OF_DIMENSIONS + d];
+        x_r1[d] = individuals[r_1 * NUM_OF_DIMENSIONS + d];
+        x_r2[d] = individuals[r_2 * NUM_OF_DIMENSIONS + d];
     }
-
+    
+    __syncthreads();
+    
+    for (int d = 0; d < NUM_OF_DIMENSIONS; d++) {
+        mutants[i * NUM_OF_DIMENSIONS + d] = base[d] + F * (x_r1[d] - x_r2[d]);
+    }
 }
