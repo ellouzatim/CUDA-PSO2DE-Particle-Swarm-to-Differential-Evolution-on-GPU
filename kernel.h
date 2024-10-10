5f7cdf99126a5fdba896a8c7d2312f599b849c7d
ellouzatim <ellouzati.mohamed84@gmail.com>
2024-10-10 15:45:14 +0200

    correction erreur

--- kernel.h
+++ kernel.h
@@ -14,10 +14,11 @@
 3: Shifted Griewank's Function
 4: Shifted Sphere's Function
 */
-const int SELECTED_OBJ_FUNC = 0;
+const int SELECTED_OBJ_FUNC = 1;
 
 const int NUM_OF_PARTICLES = 512;
 const int NUM_OF_POPULATION = 512;
+const int NUM_OF_DIMENSIONS = 10;
 
 const int MAX_ITER = NUM_OF_DIMENSIONS * pow(10, 4);
 const float START_RANGE_MIN = -5.12f;
@@ -26,10 +27,10 @@ const float OMEGA = 0.5;
 const float c1 = 1.5;
 const float c2 = 1.5;
 const float phi = 3.1415;
+const float F = 0.5f; 
 
 // =====    DE  =====
 // Number of dimension
-const int NUM_OF_DIMENSIONS = 3;
 // Crossover rate for DE
 const float CR = 0.5;   
 
