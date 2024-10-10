3c09670aad837a5ed467973fe08a7e142a180655
ellouzatim <ellouzati.mohamed84@gmail.com>
2024-10-10 15:55:10 +0200

    maj suppresion

--- kernel.h
+++ kernel.h
@@ -19,10 +19,8 @@ const int SELECTED_OBJ_FUNC = 1;
 
 // =====    DE  =====
 // Number of dimension
-const int NUM_OF_DIMENSIONS = 3;
 // Crossover rate for DE
 const float CR = 0.5;   
-const int NUM_OF_PARTICLES = 512;
 const int NUM_OF_POPULATION = 512;
 const int NUM_OF_DIMENSIONS = 10;
 
@@ -30,10 +28,6 @@ const int MAX_ITER = NUM_OF_DIMENSIONS * pow(10, 4);
 
 const float START_RANGE_MIN = -5.12f;
 const float START_RANGE_MAX = 5.12f;
-const float OMEGA = 0.5;
-const float c1 = 1.5;
-const float c2 = 1.5;
-const float phi = 3.1415;
 const float F = 0.5f; 
 
 
@@ -44,5 +38,3 @@ float getRandomClamped();
 float host_fitness_function(float x[]);
 
 extern "C" void cuda_de(float *population, float* evaluation);
-
-extern "C" void cuda_pso(float *positions, float *velocities, float *pBests, float *gBest);
